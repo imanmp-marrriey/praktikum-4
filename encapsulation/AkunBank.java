994b0f8d77a748c4f46f61440ddc3f5c0a1fe52e
imanmp-marriey <imanmardiyantipratama@gmail.com>
2025-10-02 00:57:09 +0700

    Menambahkan file AkunBank.java di folder encapsulation

--- encapsulation/AkunBank.java
+++ encapsulation/AkunBank.java
@@ -1,36 +1,40 @@
 public class AkunBank {
-
     private int saldo;
 
+    // constructor
     public AkunBank(int saldo) {
         this.saldo = saldo;
     }
 
+    // getter saldo
+    public int getSaldo() {
+        return saldo;
+    }
+
+    // setter saldo
     public void setSaldo(int saldo) {
         this.saldo = saldo;
     }
 
-    public int getSaldo() {
-        return this.saldo;
+    // menabung
+    public void menabung(int jumlah) {
+        if (jumlah > 0) {
+            saldo += jumlah;
+            System.out.println("Berhasil menabung Rp " + jumlah);
+        } else {
+            System.out.println("Jumlah tabungan harus lebih dari 0");
+        }
     }
 
-    // Method menabung
-    public int menabung(int jumlah) {
-        this.saldo += jumlah;
-        System.out.println("Berhasil menabung Rp" + jumlah);
-        return this.saldo;
-    }
-
-    // Method tarik tunai
-    public int tarikTunai(int jumlah) {
+    // tarik tunai
+    public void tarikTunai(int jumlah) {
         if (jumlah <= 0) {
-            System.out.println("Saldo penarikan harus lebih dari 0");
-        } else if (this.saldo - jumlah < 10) {
+            System.out.println("Jumlah penarikan harus lebih dari 0");
+        } else if (saldo - jumlah < 10) { // saldo minimal 10
             System.out.println("Penarikan gagal! Saldo minimal mengendap Rp10");
         } else {
-            this.saldo -= jumlah;
-            System.out.println("Berhasil tarik tunai Rp" + jumlah);
+            saldo -= jumlah;
+            System.out.println("Berhasil tarik tunai Rp " + jumlah);
         }
-        return this.saldo;
     }
 }
