26a9460447f8a2ea831f22085ec194e28ee0db45
imanmp-marriey <imanmardiyantipratama@gmail.com>
2025-10-02 00:59:06 +0700

    Menambahkan file AkunBankTest.java di folder encapsulation

--- encapsulation/AkunBankTest.java
+++ encapsulation/AkunBankTest.java
@@ -1,23 +1,41 @@
 public class AkunBankTest {
     public static void main(String[] args) {
-        
-        AkunBank akun1 = new AkunBank(100);
-        AkunBank akun2 = new AkunBank(200);
-        AkunBank akun3 = new AkunBank(300);
 
-        System.out.println("Saldo akun1 : " + akun1.getSaldo());
-        System.out.println("Saldo akun2 : " + akun2.getSaldo());
-        System.out.println("Saldo akun3 : " + akun3.getSaldo());
+        // -------------------------------
+        // UJI COBA AKUN 1
+        // -------------------------------
+        AkunBank akun1 = new AkunBank(0);
+        System.out.println("=== Akun 1 ===");
+        akun1.setSaldo(100);
+        System.out.println("Saldo awal akun1 : " + akun1.getSaldo());
 
-        // Uji coba menabung
         akun1.menabung(50);
-        System.out.println("Saldo akun1 setelah menabung: " + akun1.getSaldo());
+        System.out.println("Saldo akun1 setelah menabung : " + akun1.getSaldo());
 
-        // Uji coba tarik tunai
+        akun1.tarikTunai(30);
+        System.out.println("Saldo akun1 setelah tarik tunai : " + akun1.getSaldo());
+
+        // -------------------------------
+        // UJI COBA AKUN 2
+        // -------------------------------
+        AkunBank akun2 = new AkunBank(200);
+        System.out.println("\n=== Akun 2 ===");
+        System.out.println("Saldo awal akun2 : " + akun2.getSaldo());
+
+        akun2.menabung(100);
+        System.out.println("Saldo akun2 setelah menabung : " + akun2.getSaldo());
+
         akun2.tarikTunai(50);
-        System.out.println("Saldo akun2 setelah tarik tunai: " + akun2.getSaldo());
+        System.out.println("Saldo akun2 setelah tarik tunai : " + akun2.getSaldo());
 
-        akun3.tarikTunai(500); // coba gagal karena saldo kurang
-        System.out.println("Saldo akun3 setelah gagal tarik tunai: " + akun3.getSaldo());
+        // -------------------------------
+        // UJI COBA AKUN 3
+        // -------------------------------
+        AkunBank akun3 = new AkunBank(300);
+        System.out.println("\n=== Akun 3 ===");
+        System.out.println("Saldo awal akun3 : " + akun3.getSaldo());
+
+        akun3.tarikTunai(295); // harus gagal karena harus menyisakan minimal 10
+        System.out.println("Saldo akun3 : " + akun3.getSaldo());
     }
 }
