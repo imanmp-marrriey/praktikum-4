857f2f024699d99b7bc1d4ac672502f7c70f1e68
imanmp-marriey <imanmardiyantipratama@gmail.com>
2025-10-02 23:54:48 +0700

    Menambahkan file AkunbankSetterGetter.java di folder encapsulation

---
+++ encapsulation/AkunBankSetterGetter.java
@@ -0,0 +1,26 @@
+public class AkunBank {
+    private String namaPemilik;
+    private double saldo;
+
+    // constructor
+    public AkunBank(String namaPemilik, double saldo) {
+        this.namaPemilik = namaPemilik;
+        this.saldo = saldo;
+    }
+
+    // getter & setter namaPemilik
+    public String getNamaPemilik() {
+        return namaPemilik;
+    }
+    public void setNamaPemilik(String namaPemilik) {
+        this.namaPemilik = namaPemilik;
+    }
+
+    // getter & setter saldo
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
