778ba6e2dc89d42e4dd920bac086c4dfe47caa10
imanmp-marriey <imanmardiyantipratama@gmail.com>
2025-10-02 23:57:06 +0700

    Menambahkan file AkunbankTesSetterGetter.java di folder encapsulation

--- encapsulation/AkunBankTestSetterGetter.java
+++ encapsulation/AkunBankTestSetterGetter.java
@@ -1,3 +1,16 @@
-public class AkunBankTestSetterGetter {
-    
+public class AkunBankTest {
+    public static void main(String[] args) {
+        AkunBank akun1 = new AkunBank("Iman", 100000);
+
+        System.out.println("Pemilik: " + akun1.getNamaPemilik());
+        System.out.println("Saldo: Rp" + akun1.getSaldo());
+
+        // ubah data pakai setter
+        akun1.setNamaPemilik("Jidan");
+        akun1.setSaldo(200000);
+
+        System.out.println("\nSetelah diubah:");
+        System.out.println("Pemilik: " + akun1.getNamaPemilik());
+        System.out.println("Saldo: Rp" + akun1.getSaldo());
+    }
 }
